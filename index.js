e10d35584b0ece4cd35e7c2c95ee4a9288e0282c
Will B <willhbjr@gmail.com>
2021-02-19 18:42:39 +0000

    Completed steps 4 and 5

--- index.js
+++ index.js
@@ -39,4 +39,14 @@ console.log(shuttleAltitude);
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
+
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+console.log(`The shuttle gained an altitude of ${shuttleAltitude}km.`);
+
+if (shuttleAltitude >= 2000){
+  console.log("Orbit achieved!");
+}
+else{
+  console.log("Failed to reach orbit.")
+}
